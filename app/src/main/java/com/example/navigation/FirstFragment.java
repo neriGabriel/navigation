420fc67b11bf782c3ec7f1d7d60f09dfb80b569e
Gabriel Neri <neri.gabriel78@gmail.com>
2020-05-21 21:55:55 -0300

    sharedPreferences

--- app/src/main/java/com/example/navigation/FirstFragment.java
+++ app/src/main/java/com/example/navigation/FirstFragment.java
@@ -1,5 +1,8 @@
 package com.example.navigation;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 
 /**
@@ -19,6 +25,7 @@ import android.widget.Button;
  */
 public class FirstFragment extends Fragment {
 
+    //Ver sobre preference fragment android
      //Primeiramente para usar o navigation é necessário fazer o import das bibliotecas nos build.gradles
      /*
      No build.gradle (Project)(Dependencies):
@@ -34,10 +41,28 @@ public class FirstFragment extends Fragment {
 
      //Faço a referencia do botão que vai "triggar" a navigation
     Button btnNavigate;
+
+    //Faço referencia ao sharedpreferences
+    SharedPreferences sharedPreferences;
+    String sharedPrefFileName = "sharedfile";
+
+    //Somente para ficar melhor a obtenção do sharedprefences
+    Activity activity;
+
+    public static final String  EDIT_TEXT_KEY = "edit_text_key";
+    public static final String  SEEK_BAR_KEY = "seek_bar_key";
+    public static final String  SWITCH_CONFIGURACAO_FIRST_KEY = "switch_configuracao_first";
+    public static final String  SWITCH_CONFIGURACAO_SECOND_KEY = "switch_configuracao_second";
+
+
+    EditText editTextValor;
+    Switch switchConfig1;
+    Switch switchConfig2;
+    SeekBar seekBar;
+    Button btnReset;
     public FirstFragment() {
         // Required empty public constructor
     }
-
 
     //Crio minha view normal, fazendo a referencia do botão
     @Override
@@ -47,6 +72,30 @@ public class FirstFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_first, container, false);
 
         this.btnNavigate = v.findViewById(R.id.btnNewFragment);
+        this.editTextValor = v.findViewById(R.id.editTextValor);
+        this.switchConfig1 = v.findViewById(R.id.switchConfig1);
+        this.switchConfig2 = v.findViewById(R.id.switchConfig2);
+        this.seekBar = v.findViewById(R.id.seekBar);
+        this.btnReset = v.findViewById(R.id.button);
+        this.btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetSharedPreferences();
+            }
+        });
+
+        //Inicio o sharedpreferences
+        //Quando utlizamos sharedPreferences é recomendado fazer todos os "salvamentos" no onPause.
+        this.activity = this.getActivity();
+        this.sharedPreferences = this.activity.getSharedPreferences(this.sharedPrefFileName, Context.MODE_PRIVATE);
+
+
+        //Pegando a informação do sharedpreferences
+        //O sharedpreferences salva mesmo depois que o app é destruido, normalmente é utilizado em configurações
+        this.editTextValor.setText(sharedPreferences.getString(this.EDIT_TEXT_KEY, ""));
+        this.switchConfig1.setChecked(sharedPreferences.getBoolean(this.SWITCH_CONFIGURACAO_FIRST_KEY, false));
+        this.switchConfig2.setChecked(sharedPreferences.getBoolean(this.SWITCH_CONFIGURACAO_SECOND_KEY, false));
+        this.seekBar.setProgress(sharedPreferences.getInt(this.SEEK_BAR_KEY, 0));
 
         return v;
     }
@@ -66,5 +115,37 @@ public class FirstFragment extends Fragment {
                 Navigation.findNavController(view).navigate(action);
             }
         });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //Acessando o editor do shared preferences
+        //Sempre que mexer com shared preferences é necessário acessar um editor
+        SharedPreferences.Editor sharedEditor = sharedPreferences.edit();
+
+        //Os métodos são iguais do bundle
+        //EDIT_TEXT_KEY
+        //SEEK_BAR_KEY
+        //SWITCH_CONFIGURACAO_FIRST_KEY
+        //SWITCH_CONFIGURACAO_SECOND_KEY
+        sharedEditor.putString(this.EDIT_TEXT_KEY, this.editTextValor.getText().toString());
+        sharedEditor.putBoolean(this.SWITCH_CONFIGURACAO_FIRST_KEY, this.switchConfig1.isChecked());
+        sharedEditor.putBoolean(this.SWITCH_CONFIGURACAO_SECOND_KEY, this.switchConfig2.isChecked());
+        sharedEditor.putInt(this.SEEK_BAR_KEY, this.seekBar.getProgress());
+
+        //apply => assincrono
+        //commit => sincrono
+        //Geralmente quando faz salvamente de info persistentes utilizamos a execução assincrona
+        sharedEditor.apply();
+    }
+
+    private void resetSharedPreferences() {
+        SharedPreferences.Editor sharedEditor = sharedPreferences.edit();
+        sharedEditor.clear();
+        this.editTextValor.setText("");
+        this.switchConfig1.setChecked(false);
+        this.switchConfig2.setChecked(false);
+        this.seekBar.setProgress(0);
     }
 }
--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
+    /* SHAREDPREFERENCES
+    * O NAVIGATEUP VOLTA UM ESTADO NA STACK. O BOTÃO BACK CORTA O FLUXO
     * Não esquecer de adicionar o componente navigation via ui, android resources -> opção navigation
     * adicionar as view e etc.
     * */
+
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        * Setando a toolbar/icone de retorno
+        * Procuro o controller e a fragment principal e seto na navigationui
+        * */
+        this.navController = Navigation.findNavController(this, R.id.nav_controller_fragment);
+        NavigationUI.setupActionBarWithNavController(this, this.navController);
+    }
+
+    /*
+    * Necessário ter
+    * */
+    @Override
+    public boolean onSupportNavigateUp() {
+        /*
+        * Retorna o navigationui
+        * */
+        return NavigationUI.navigateUp(this.navController, (DrawerLayout) null);
     }
 }
